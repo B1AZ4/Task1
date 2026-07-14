25b8acb17a1da7869272b105bc1bdc238b071401
Смоликов Роман <romansmolikov4@gmail.com>
2026-07-15 02:17:40 +0500

    buffer.c:add, remove

--- Lib/src/buffer.c
+++ Lib/src/buffer.c
@@ -6,6 +6,7 @@
 */
 #include "buffer.h"
 #include <stdlib.h>
+#include <string.h>
 
 Buffer* buffer_create(size_t capacity) {
     Buffer *buf = (Buffer*)malloc(sizeof(Buffer));
@@ -36,4 +37,31 @@ const uint8_t* buffer_data(const Buffer *buf) {
 
 size_t buffer_size(const Buffer* buf) {
     return buf->size;
+}
+
+
+void buffer_add(Buffer *buf, const uint8_t *data, size_t len) {
+    if (buf->size + len > buf->capacity) {
+        if (buf->offset > 0) {
+            memmove(buf->data, buf->data + buf->offset, buf->size);
+            buf->offset = 0;
+        }
+        if (buf->size + len > buf->capacity) {
+            len = buf->capacity - buf->size;
+        }
+    }
+
+    memcpy(buf->data + buf->offset + buf->size, data, len);
+    buf->size += len;
+}
+
+void buffer_remove(Buffer *buf, size_t len) {
+    if (len >= buf->size) {
+        buf->size = 0;
+        buf->offset = 0;
+    }
+    else {
+        buf->offset += len;
+        buf->size -= len;
+    }
 }
