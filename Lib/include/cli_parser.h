6d253cb4dadd9b74fc09809f61295f6ecb3023c5
Смоликов Роман <romansmolikov4@gmail.com>
2026-07-15 22:11:43 +0500

    cli_parser.h

--- Lib/include/cli_parser.h
+++ Lib/include/cli_parser.h
@@ -4,4 +4,13 @@ cli_parser.h - заголовочный файл модуля cli_parser.c
 Смоликов Роман
 МК-101
 */
-#pragma once
+#ifndef CLI_PARSER_H
+#define CLI_PARSER_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+int hex_decode(const char *hex_str, uint8_t **out, size_t *out_len);
+void hex_free(uint8_t *data);
+
+#endif
